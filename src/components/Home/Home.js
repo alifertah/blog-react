cd96730430d2e1ef36bb01b401cd042809e09fb9
Ali Fertah <85651433+alifertah@users.noreply.github.com>
2022-04-22 00:50:58 +0000

    creting a div using React.createlement

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import Topbar from 'global/Topbar'
-import Footer from 'global/Footer'
 import { Link } from 'react-router-dom'
+
+const Newpost = () => {
+  return React.createElement(
+    "div",
+    {style:{color:"red"}, id: 'someId', className: "someClass"},
+    "hello world!",
+  );
+}
+
+
+const TryRequire = (path) => {
+  try {
+   return require(`/post/1337`);
+  } catch (err) {
+   return null;
+  }
+};
+
 
 function Home() {
   return (
     <div>
         <ul>
             <li><Link to="/post/1337">1337</Link><span>07 Apr 2022</span></li>
+            <TryRequire />
         </ul>
     </div>
   )
