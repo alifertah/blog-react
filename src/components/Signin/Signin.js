64e0fae3aa86669d904d8e39c7aa4a2779dd5ff8
Ali Fertah <alifertah42@gmail.com>
2022-04-30 21:56:34 +0200

    :wrench: signin page done

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Signin() {
   return (
-    <div>Signin</div>
+    <div className='Signin-com'>
+                <h1>Sign in</h1>
+      <div class="form">
+        <form class="login-form">
+        <input type="text" placeholder="username" />
+        <input type="text" placeholder="email" />
+        <input type="text" placeholder="phone number" />
+          <input type="password" placeholder="password" />
+          <input type="password" placeholder="confirm password" />
+          <button>login</button>
+          <p class="message">
+            Not registered? <Link to="/Signin">Create an account</Link>
+          </p>
+        </form>
+      </div>
+    </div>
   )
 }
 
